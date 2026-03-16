0c23e016e07b5be29e28c56d073cd1c0c577d17b
ericvoi <ericvoisin1@gmail.com>
2026-03-15 22:10:53 -0600

    Subsys error documentation

--- Application/Inc/error/error_subsys.h
+++ Application/Inc/error/error_subsys.h
@@ -41,12 +41,35 @@ typedef enum {
 
 /* Exported functions prototypes ---------------------------------------------*/
 
+/**
+ * @brief Disables a subsystem by ID
+ * 
+ * @param subsys The subsystem to disable
+ */
 void ErrorSubsys_Disable(SubSystemId_t subsys);
 
+/**
+ * @brief Sets flag that a subsystem should reset
+ * 
+ * @param subsys The subsystem to reset
+ */
 void ErrorSubsys_RequestReset(SubSystemId_t subsys);
 
+/**
+ * @brief Get the current status of a subsystem
+ * 
+ * @param subsys Subsystem to get the status of
+ * @return SubSystemStatus_t SUBSYS_DISABLE Abort any and all operations in subsystem
+ *                           SUBSYS_RESET Reset the subsystem
+ *                           SUBSYS_PROCEED Continue normal operation of subsystem
+ */
 SubSystemStatus_t ErrorSubsys_CurrentStatus(SubSystemId_t subsys);
 
+/**
+ * @brief Clears reset condition in the subsystem
+ * 
+ * @param subsys The subsystem to clear the reset condition of
+ */
 void ErrorSubsys_ClearReset(SubSystemId_t subsys);
 
 /* Private defines -----------------------------------------------------------*/
